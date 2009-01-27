fe1c6cf66614437daeeacba72bfe69726d6edef3
NeilCrosby <neil@thetenwordreview.com>
2009-01-27 05:11:36 -0700

    Removed old documentation that no longer applied.

--- suite/TheCodeTrainBaseValidatorTestCase.php
+++ suite/TheCodeTrainBaseValidatorTestCase.php
@@ -2,8 +2,7 @@
 
 /**
  * TestCase class to inherit from to allow Unit Testing based checking of
- * HTML validity.  Right now, this is only set up to test modules, not full 
- * pages.  I'll fix this to do anything and everything soon.
+ * HTML validity.
  *
  * @author  Neil Crosby <neil@neilcrosby.com>
  * @license Creative Commons Attribution-Share Alike 3.0 Unported 
