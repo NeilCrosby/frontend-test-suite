f93339184b50322ba4163ea2ae8c46b801ef96e0
NeilCrosby <neil@thetenwordreview.com>
2009-05-14 02:38:30 -0700

    Added commented out options so that people can see what's possible.

--- examples/ExampleTestSuite.php
+++ examples/ExampleTestSuite.php
@@ -8,6 +8,9 @@
       return parent::suite( array(
         'html'  =>  array(
           'validator' => 'http://validator.w3.org/check',
+          // 'doctype_override' => '<!DOCTYPE HTML SYSTEM "http://site/doctype.dtd">',
+          // 'document_section' =>  TheCodeTrainHtmlValidator::HTML_CHUNK, 
+          // 'document_section_position' =>  TheCodeTrainHtmlValidator::POSITION_HEAD
           'tests'     => array(
             'http://yahoo.com/'
           )
