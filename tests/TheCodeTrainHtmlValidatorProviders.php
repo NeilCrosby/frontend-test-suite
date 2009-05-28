826dc6114c72575e2da78f25564e4c94206f97b0
Steve Webster <spjwebster@gmail.com>
2009-05-29 06:03:00 +0800

    Fixed the mixed up DEFAULT_VALIDATOR_URL and DEFAULT_ASSETS_BASE_URL values in the HtmlValidatorProvider class.
    Signed-off-by: Neil Crosby <neil@thetenwordreview.com>

--- tests/TheCodeTrainHtmlValidatorProviders.php
+++ tests/TheCodeTrainHtmlValidatorProviders.php
@@ -6,8 +6,8 @@
  *          http://creativecommons.org/licenses/by-sa/3.0/
  **/
 class TheCodeTrainHtmlValidatorProviders {
-    const DEFAULT_ASSETS_BASE_URL = 'http://htmlvalidator/check';
-    const DEFAULT_VALIDATOR_URL = 'http://dtd:8888/';
+    const DEFAULT_VALIDATOR_URL = 'http://htmlvalidator/check';
+    const DEFAULT_ASSETS_BASE_URL = 'http://dtd:8888/';
     const DTD_401_STRICT_EXTENDED = '<!DOCTYPE HTML SYSTEM "{assets-base-url}/dtd/401_strict_extended.dtd">';
     
     public static function fileProvider($path) {
